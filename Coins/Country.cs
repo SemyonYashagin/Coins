69976fc947e5f511969b084de097429681944b42
Semyon <phend38@gmail.com>
2022-10-27 15:06:33 +0300

    Initial commit

---
+++ Coins/Country.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coins
+{
+    public class Country
+    {
+        public string Name;
+        public int x1;
+        public int y1;
+        public int x2;
+        public int y2;
+        public static int NOTDONE = 1000000000;
+        public int isComplete;
+        public int[,] grid;
+
+        public Country(string name, int xl, int yl, int xh, int yh) // init the country
+        {
+            Name = name;
+            x1 = xl;
+            y1 = yl;
+            x2 = xh;
+            y2 = yh;
+            isComplete = NOTDONE;
+            grid = new int[x2 - x1 + 1,y2 - y1 + 1]; // all elements are null (0)
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    grid[i, j] = -1;
+                }
+            }
+        }
+
+        public void isCountryCompleted()
+        {
+            int max = 1;
+            for (int i = 0; i < grid.GetLength(0); i++) //circling of all cities in the country
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+
+                    if (grid[i,j] == 0) 
+                        return;
+                    else 
+                        max = Math.Max(max, grid[i,j]);
+                }
+            }
+        }
+
+        public void Update(City[,] city, int day)
+        {
+            for (int i = 0; i < x2 - x1 + 1; i++)
+                for (int j = 0; j < y2 - y1 + 1; j++)
+                    if (grid[i,j] == 0 && city[x1 + i,y1 + j].done())
+                        grid[i,j] = day;
+        }
+
+
+    }
+}
---
+++ Coins/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coins
+{
+    public class Program
+    {
+        public static int day;
+        public static int n; //the count of countries
+
+        static void Main(string[] args)
+        {
+
+            n = int.Parse(Console.ReadLine()); //read the first line
+            int k = 1; // count of a case
+
+            while (n!=0)
+            {
+                co
+            }
+
+        }
+    }
+}

365fb679f6213766a906c531ca8332c5228d4560
Semyon <phend38@gmail.com>
2022-10-31 20:16:06 +0300

    Realized the moving the coins to other cities. Fixed bugs when the user has typing the input data.

--- Coins/Country.cs
+++ Coins/Country.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 namespace Coins
 {
-    public class Country
+    public class Country: IComparable<Country>
     {
         public string Name;
         public int x1, y1, x2, y2; //coordinates of cites: left buttom and top right
-        public static int maxValue = 1000000000;//the maximum of operations (no more days then MaxValue)
+        public static int maxValue = int.MaxValue;//the maximum of operations (no more days then MaxValue)
         public int numberOfDays;
-        public int[,] grid;
+        public int[,] finishArray;
 
         public Country(string name, int xl, int yl, int xh, int yh) // init the country
         {
@@ -23,43 +23,43 @@ namespace Coins
             x2 = xh;
             y2 = yh;
             numberOfDays = maxValue;
-            grid = new int[x2 - x1 + 1,y2 - y1 + 1]; // all elements are null (0)
-            for (int i = 0; i < grid.GetLength(0); i++)
+            finishArray = new int[x2 - x1 + 1,y2 - y1 + 1]; // all elements are null (0)
+            for (int i = 0; i < finishArray.GetLength(0); i++)
             {
-                for (int j = 0; j < grid.GetLength(1); j++)
+                for (int j = 0; j < finishArray.GetLength(1); j++)
                 {
-                    grid[i, j] = -1;
+                    finishArray[i, j] = -1;
                 }
             }
         }
         public void isCountryCompleted()
         {
             int max = 0;
-            for (int i = 0; i < grid.GetLength(0); i++) //circling of all cities in the country
+            for (int i = 0; i < finishArray.GetLength(0); i++) //circling of all cities in the country
             {
-                for (int j = 0; j < grid.GetLength(1); j++)
+                for (int j = 0; j < finishArray.GetLength(1); j++)
                 {
 
-                    if (grid[i,j] == -1) 
+                    if (finishArray[i,j] == -1) 
                         return;
                     else 
-                        max = Math.Max(max, grid[i,j]);
+                        max = Math.Max(max, finishArray[i,j]);
                 }
             }
             numberOfDays = max;
         }
         public void Update(City[,] city, int day)
         {
-            for (int i = 0; i < grid.GetLength(0); i++)
-                for (int j = 0; j < grid.GetLength(1); j++)
-                    if (grid[i,j] == -1 && city[x1 + i,y1 + j].isDone())
-                        grid[i,j] = day;
+            for (int i = 0; i < finishArray.GetLength(0); i++)
+                for (int j = 0; j < finishArray.GetLength(1); j++)
+                    if (finishArray[i,j] == -1 && city[x1 + i,y1 + j].isDone())
+                        finishArray[i,j] = day;
         }
         public bool isComplete()//the county is comlete or not
         {
             return numberOfDays < maxValue;
         }
-        public int compareTo(Country other)// sorting countries by alphabet
+        public int CompareTo(Country other)// should do this for sorting countries by alphabet (IComparable<Country)
         {
             if (this.numberOfDays != other.numberOfDays)
                 return this.numberOfDays - other.numberOfDays;
--- Coins/Program.cs
+++ Coins/Program.cs
@@ -11,19 +11,25 @@ namespace Coins
     {
         public static int day;
         public static int n; //the count of countries
-        public static int[] dx = { 0, -1, 1, 0 };
-        public static int[] dy = { -1, 0, 0, 1 };
+        public static int[] dx = { 0, -1, 1, 0 };// on "x" coordinate for moving coins to neighbor
+        public static int[] dy = { -1, 0, 0, 1 };// on "y" coordinate for moving coins to neighbor
         static void Main(string[] args)
-        {
-
-            int.TryParse(Console.ReadLine(), out n); //the number of countries
+        {   
+            Console.WriteLine("Input the number of countries \"n\" (1<=n<=20): ");            
+            while(!int.TryParse(Console.ReadLine(), out n)) //the number of countries
+            {
+                Console.WriteLine("the \"n\" should be the number");
+            }
+            
+            if (n == 1) { day = 0; } else { day = 1; } // if a country includes only one city day = 0
             int count = 1; // count of a case
 
+            //while an user don't input n = 0 
             while (n!=0)
             {
-                int x1, x2,y1, y2;
+                int x1, x2,y1, y2;// city's coordinates
                 Country[] list = new Country[n];
-                City[,] grid = new City[11,11];
+                City[,] grid = new City[11,11]; //11x11 - is maximum of cities
 
                 for (int i = 0; i < 11; i++)
                     for (int j = 0; j < 11; j++)
@@ -31,45 +37,40 @@ namespace Coins
 
                 for (int i = 0; i < n; i++)
                 {
+                    //parsing the values s, x1,y1,x2,y2
                     string[] str = Console.ReadLine().ToString().Split(' ');
-                    //Char.TryParse(Console.Read().ToString(), out x1);
                     string s = str[0];
                     int.TryParse(str[1], out x1);
                     int.TryParse(str[2], out y1);
                     int.TryParse(str[3], out x2);
                     int.TryParse(str[4], out y2);
 
-                    list[i] = new Country(s, x1, y1, x2, y2);
-                    init(grid, list[i], i, n);
+                    list[i] = new Country(s, x1, y1, x2, y2);//the array of countries
+                    Init(grid, list[i], i, n);
                 }
 
-                // Annoying special case.
-                if (n == 1) 
-                    day = 0;
-                else 
-                    day = 1;
-
-                // Simulate.
-                while (!done(list))
+                // Do this while the countries isn't completed
+                while (!Done(list))
                 {
-                    grid = updateCoins(grid);
-                    updateStatus(grid, list);
+                    grid = TransferCoins(grid);
+                    UpdateStatus(grid, list);
                     day++;
                 }
 
-                //Array.Sort(list);
+                // need to fix a sorting of array
+                Array.Sort(list);
                 Console.WriteLine("Case Number " + count);
                 for (int i = 0; i < n; i++)
-                    Console.WriteLine(list[i]);
+                    Console.WriteLine(list[i].ToString());
 
                 count++;
                 int.TryParse(Console.ReadLine(), out n); // repeat the input
             }
         }
 
-        public static void updateStatus(City[,] grid, Country[] list)
+        public static void UpdateStatus(City[,] grid, Country[] list)
         {
-            // First update completion dates per city, then check if countries are complete.
+            // Update data of a city, then check if country are complete or not
             for (int i = 0; i < n; i++)
             {
                 list[i].Update(grid, day);
@@ -77,10 +78,9 @@ namespace Coins
             }
         }
 
-        public static City[,] updateCoins(City[,] grid)
+        public static City[,] TransferCoins(City[,] grid)
         {
-
-            // Store a new copy for the next day.
+            // Save a new copy for the next day
             City[,] next = new City[11,11];
             for (int i = 0; i < 11; i++)
             {
@@ -91,64 +91,61 @@ namespace Coins
                 }
             }
 
-            // Go through the whole grid.
+            // Go on the whole city grid
             for (int i = 0; i < 11; i++)
             {
                 for (int j = 0; j < 11; j++)
                 {
                     if (grid[i,j] != null)
                     {
-
-                        // Calculate coins that go to neighbors.
+                        // Calculate how many coins shall go to neighbors
                         int[] move = new int[n];
                         for (int k = 0; k < n; k++)
                             move[k] = grid[i,j].coins[k] / 1000;
 
-                        // Move coins to neighbors.
+                        // Move coins to neighbors
                         int numNeighbors = 0;
                         for (int k = 0; k < dx.Length; k++)
                         {
-                            int myx = i + dx[k];
-                            int myy = j + dy[k];
-                            if (inbounds(myx, myy) && grid[myx,myy] != null)
+                            int myx = i + dx[k];//the x coord of the neighbor
+                            int myy = j + dy[k];//the y coord of the neighbor
+                            if (InBounds(myx, myy) && grid[myx,myy] != null)
                             {
                                 numNeighbors++;
                                 for (int z = 0; z < n; z++)
-                                    next[myx,myy].coins[z] += move[z];
+                                    next[myx,myy].coins[z] += move[z];//sum = what we have in current + what we have from other neighbor
                             }
                         }
 
-                        // Update coins left for you.
+                        // Update coins for neighbor
                         for (int k = 0; k < n; k++)
-                            next[i,j].coins[k] += (grid[i,j].coins[k] - numNeighbors * move[k]);
+                            next[i,j].coins[k] += (grid[i,j].coins[k] - numNeighbors * move[k]);// - how many coins we sent to others
                     }
                 }
             }
-
             return next;
         }
-
-        public static bool inbounds(int x, int y)
+        //return true if a neighbor (city) not out of gri
+        public static bool InBounds(int x, int y)
         {
             return 0 <= x && x < 11 && 0 <= y && y < 11;
         }
 
-        // Initialize the city grid based on this country which has the given index.
-        public static void init(City[,] grid, Country usa, int index, int numCountries)
+        // In this method we put the country object on the grid
+        public static void Init(City[,] grid, Country country, int index, int numCountries)
         {
-            for (int i = usa.x1; i <= usa.x2; i++)
-                for (int j = usa.y1; j <= usa.y2; j++)
+            for (int i = country.x1; i <= country.x2; i++)
+                for (int j = country.y1; j <= country.y2; j++)
                     grid[i,j] = new City(numCountries, i, j, index);
         }
 
-        // Returns true iff all countries are done.
-        public static bool done(Country[] list)
+        // Returns true if all countries are done
+        public static bool Done(Country[] list)
         {
             for (int i = 0; i < n; i++)
                 if (!list[i].isComplete())
                     return false;
             return true;
         }
-
     }
 }
